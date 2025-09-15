2ab8d129d3c2a23e69950317ccddceb7d29fe43f
Donagala-Tejaswi <tejaswidongala@gmail.com>
2025-09-15 06:26:36 +0530

    trail& error

--- Subtraction.java
+++ Subtraction.java
@@ -1,12 +1,14 @@
 public class Subtraction {
     public static int subtract(int a, int b) {
         return a - b;
+        
     }
 
     public static void main(String[] args) {
         int x = 10;
         int y = 4;
+        System.out.println("adding branch");
         System.out.println("Subtraction: " + subtract(x, y));
-        System.out.println("Hrllo tejaswi");
+        System.out.println("Heello tejaswi");
     }
 }
