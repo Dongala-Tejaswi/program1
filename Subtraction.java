14d61a6ac7dfbc313668410309b154c8b53e63ee
Donagala-Tejaswi <tejaswidongala@gmail.com>
2025-09-15 05:46:53 +0530

    Added mini stubraction program

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+public class Subtraction {
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 4;
+        System.out.println("Subtraction: " + subtract(x, y));
+    }
+}
